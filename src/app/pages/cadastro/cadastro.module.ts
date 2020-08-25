0b3ed3bdd7fa8cd5c5209588f611524159196425
daniel00nt01 <danielnt2002@gmail.com>
2020-08-25 18:45:17 -0300

    task registration page

--- src/app/pages/cadastro/cadastro.module.ts
+++ src/app/pages/cadastro/cadastro.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CadastroPageRoutingModule } from './cadastro-routing.module';
 
 import { CadastroPage } from './cadastro.page';
+import { ApiDjangoService } from 'src/app/services/api-django.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     CadastroPageRoutingModule
   ],
-  declarations: [CadastroPage]
+  declarations: [CadastroPage],
+  providers: [ApiDjangoService]
 })
 export class CadastroPageModule {}
--- src/app/pages/cadastro/cadastro.page.ts
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {NavController} from '@ionic/angular';
+import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import Swal from 'sweetalert2';
 import {RegistrarpedidosService} from '../../services/registrarpedidos.service';
 
 @Component({
@@ -20,14 +21,32 @@ export class CadastroPage implements OnInit {
 
   ngOnInit() {
     this.form = this.formBuilder.group({
+      title: new FormControl('', [Validators.required]),
       description: new FormControl('', [Validators.required]),
-      payment: new FormControl('', [Validators.required]),
+      payment: new FormControl('', [Validators.required, Validators.min(0)]),
     });
   }
-
+  
+  getErrorMessage(form: FormGroup, field) {
+    if (form.get(field)) {
+      return form.get(field).hasError('required')
+        ? 'Campo requerido'
+        : form.get(field).hasError('min')
+        ? 'Valor inválido'
+        : '';
+    }
+  }
+  
+    isInvalid(field) {
+      return (
+        !this.form.controls[field].valid &&
+        (this.form.controls[field].touched)
+      );
+    }
 
     formataEnvio() {
       const registerDataReturn = {
+        title: this.form.get('title').value,
         description: this.form.get('description').value,
         payment: this.form.get('payment').value,
         // email_client: this.form.get('email').value,
@@ -38,4 +57,27 @@ export class CadastroPage implements OnInit {
 
       return registerDataReturn;
     }
-}
+
+    cadastrar() {
+      const registerData = this.formataEnvio();
+  
+      this.registraPedidoService.registrarPedido(registerData).subscribe(
+        (response) => {
+          Swal.fire({
+            title: 'Sucesso!',
+            text: 'Tarefa cadastrada com sucesso',
+            icon: 'success'
+          }).then(() => {
+            this.navCtrl.navigateForward('/cadastro');
+          });
+        }, error => {
+          Swal.fire({
+            title: 'Erro!',
+            text: 'Erro ao cadastrar tarefa',
+            icon: 'error'
+          });
+          console.log(error);
+        }
+      );
+    }
+  }
