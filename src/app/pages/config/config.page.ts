d180bcf43e5b287d58c58855c49aa0ec95dfb127
daniel00nt01 <danielnt2002@gmail.com>
2020-08-28 21:32:27 -0300

    pagina de configuração do usuário
    sem função por enquanto

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -9,6 +9,24 @@ export class ConfigPage implements OnInit {
 
   constructor() { }
 
+  public appPages = [
+    {
+      title: 'Nome',
+     // url: 'edit-name',
+      icon: 'pencil'
+    },
+    {
+      title: 'Número de telefone',
+     // url: 'edit-phone',
+      icon: 'call'
+    },
+    {
+      title: 'E-mail',
+     // url: 'edit-mail',
+      icon: 'mail'
+    },
+  ];
+
   ngOnInit() {
   }
 
